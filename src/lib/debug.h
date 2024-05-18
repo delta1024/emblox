240cb939ca166d49805d27c2657ee88c54ec96a9
Jake Stannix <jakestannix@gmail.com>
2024-05-17 20:49:06 -0600

    pre rewrite purge

--- src/lib/debug.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LOX_DEBUG_H
-#define _LOX_DEBUG_H
-#include "chunk.h"
-
-void lox_debug_dissasemble_chunk(lox_chunk *chunk, const char *name);
-int lox_debug_dissasemble_instruction(lox_chunk *chunk, int instructuon);
-
-#endif // !_LOX_DEBUG_H
